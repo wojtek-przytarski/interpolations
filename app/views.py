18b0d86d041fdc243b46befb59f202a37648135d
Wojtek Przytarski <vojtek16@gmail.com>
2020-05-21 19:54:17 +0200

    Test integration

--- app/views.py
+++ app/views.py
@@ -1,13 +1,12 @@
 import parser
 
 from math import e, sin
-from flask import render_template, request, Flask
+from flask import render_template, request, Flask, make_response
 
 from app.exceptions import RequiredArgumentsMissing
 from app.figure_creator import FigureCreator
 from app.interpolation import get_interpolation_axes, get_original_axes
 from app.utils import check_if_request_has_required_params
-
 
 app = Flask(__name__)
 figure_creator = FigureCreator()
@@ -41,6 +40,38 @@ def home():
             "main.html", error="Error: {}".format(str(ex)), params=params
         )
     return render_template("main.html", plots=plots, params=params)
+
+
+@app.route("/api/plots", methods=["GET"])
+def plot_points():
+    params = request.args
+    try:
+        check_if_request_has_required_params(params, ["f", "a", "b", "n"])
+        formula = parser.expr(params.get("f")).compile()
+
+        a = float(params.get("a"))
+        b = float(params.get("b"))
+        n = int(params.get("n"))
+        if a >= b:
+            raise RequiredArgumentsMissing(
+                "Please provide valid parameters: a should be lower than b."
+            )
+
+        x, fx = get_original_axes(lambda x: eval(formula), a, b, n)
+        x, npv = get_interpolation_axes(lambda x: eval(formula), a, b, n)
+
+        data = {
+            'x': x,
+            'npv': npv,
+            'fx': fx,
+        }
+    except SyntaxError:
+        data = {'error': f"Error in function provided: {params.get('f')}"}
+        return make_response(data, 400)
+    except Exception as ex:
+        data = {'error': str(ex)}
+        return make_response(data, 400)
+    return make_response(data)
 
 
 def get_plots(f, a, b, n):
